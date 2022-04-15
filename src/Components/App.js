6c2429362a403f62100718e21258116f91dd4d8a
CharlesLee-23 <Coleman.hallom@Spray.com>
2022-04-15 11:52:56 -0500

    Adding bootstrap back in.

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,7 +4,7 @@ import {BigCount} from './Counter/counts.js';
 import {AddLogo} from './Logo/addLogo.js';
 import './App.css';
 import Button from 'react-bootstrap';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
